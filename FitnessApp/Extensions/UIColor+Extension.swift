243fff75788aa5231020ddcf26c98a180e65f315
strahovochka <103863168+strahovochka@users.noreply.github.com>
2024-07-03 15:00:55 +0300

    added colors and images

---
+++ FitnessApp/Extensions/UIColor+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Extension.swift
+//  FitnessApp
+//
+//  Created by Jane Strashok on 02.07.2024.
+//
+
+import UIKit
+
+extension UIColor {
+    //Primary colors
+    static let primaryRed = UIColor(named: "primaryRed")
+    static let primaryOrange = UIColor(named: "primaryOrange")
+    static let primaryYellow = UIColor(named: "primaryYellow")
+    static let primaryGray = UIColor(named: "primaryGray")
+    static let primaryWhite = UIColor(named: "primaryWhite")
+    
+    //Secondary colors
+    static let secondaryGray = UIColor(named: "secondaryGray")
+}
