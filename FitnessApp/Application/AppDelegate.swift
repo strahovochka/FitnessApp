204d101880304be69ff5c261736a72d24c8ec6eb
strahovochka <103863168+strahovochka@users.noreply.github.com>
2024-07-08 00:50:37 +0300

    added firebase

--- FitnessApp/Application/AppDelegate.swift
+++ FitnessApp/Application/AppDelegate.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 
